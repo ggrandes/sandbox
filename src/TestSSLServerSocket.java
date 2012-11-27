18da05c684a914c7f6bfbb921c867137340e0d5c
ggrandes <guillermo.grandes@gmail.com>
2012-11-27 21:22:37 +0100

    Test concept: SSL Server with auto generation of self-signed X.509

--- src/TestSSLServerSocket.java
+++ src/TestSSLServerSocket.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -13,6 +15,7 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManagerFactory;
 
 /**
  * http://stackoverflow.com/questions/1788031/how-can-i-have-multiple-ssl-certificates-for-a-java-server/1788047#1788047
@@ -28,22 +31,27 @@ public class TestSSLServerSocket {
 	 */
 	public static void main(String[] args) throws Exception {
 		char[] pwd = "changeit".toCharArray();
-		// Init SSL Factory
-		KeyStore identity = KeyStore.getInstance(KeyStore.getDefaultType());
-		identity.load(null);
 		// Generate RSA Key
 		KeyPairGenerator kgAsym = KeyPairGenerator.getInstance("RSA");
 		kgAsym.initialize(1024); // TODO: RSA { 1024, 1536, 2048 }
 		KeyPair kp = kgAsym.genKeyPair();
-		//
+		// Generate X.509 Certificate
 		X509Certificate crt = TestX509.generateCertificate("CN=Test1", kp, 365);
+		File crtFile = new File(System.getProperty("java.io.tmpdir"), "test.crt");
+		TestX509.writeCertificate(new FileOutputStream(crtFile), crt);
 		//
+		// Init SSL Factory
+		KeyStore identity = KeyStore.getInstance(KeyStore.getDefaultType());
+		identity.load(null);
 		identity.setKeyEntry("private", kp.getPrivate(), pwd, new Certificate[] { crt });
 		SSLContext ctx = SSLContext.getInstance("TLS");
 		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
 		kmf.init(identity, pwd);
-		ctx.init(kmf.getKeyManagers(), null, null);
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		tmf.init(identity);
+		ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
 		SSLServerSocketFactory factory = ctx.getServerSocketFactory();
+		//
 		SSLServerSocket listen = (SSLServerSocket) factory.createServerSocket(1991);
 		// Setup
 		List<String> protos = Arrays.asList(new String[] { 
@@ -69,13 +77,22 @@ public class TestSSLServerSocket {
 		System.out.println("Listen: " + listen);
 		while (true) {
 			SSLSocket socket = (SSLSocket) listen.accept();
-			System.out.println("New client: " + socket + " session: " + socket.getSession());
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String line = null;
-			while ((line = in.readLine()) != null) {
-				System.out.println(line);
+			try {
+				System.out.println("New client: " + socket + " session: " + socket.getSession());
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String line = null;
+				while ((line = in.readLine()) != null) {
+					System.out.println(line);
+				}
+				System.out.println("End client: " + socket + " session: " + socket.getSession());
 			}
-			System.out.println("End client: " + socket + " session: " + socket.getSession());
+			catch (Exception e) {
+				e.printStackTrace();
+				try { Thread.sleep(1000); } catch (Exception ign) {}
+			}
+			finally {
+				try { socket.close(); } catch (Exception ign) {}
+			}
 		}
 	}
 
