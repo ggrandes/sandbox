04df246bb110db92388774c7a17b971d185b325b
ggrandes <guillermo.grandes@gmail.com>
2012-12-04 09:50:21 +0100

    Fast int/long/byte[] to Hex String (left-zero-padding)

--- src/SimpleHex.java
+++ src/SimpleHex.java
@@ -1,4 +1,4 @@
-// Fast int/long to Hex String (left-zero-padding)
+// Fast int/long/byte[] to Hex String (left-zero-padding)
 public class SimpleHex {
 	private static final char[] HEX_CHARS = "0123456789abcdef".toCharArray();
 
@@ -15,6 +15,14 @@ public class SimpleHex {
 		final int len = (sb.length-1);
 		for (int i = 0; i <= len; i++) { // MSB
 			sb[i] = HEX_CHARS[((int)(input >>> ((len - i)<<2))) & 0xF];
+		}
+		return new String(sb);
+	}
+	public static String bytesAsHex(final byte[] input) {
+		final char[] sb = new char[input.length<<1];
+		for (int j = 0; j < input.length; j++) {
+			sb[(j<<1)|0] = HEX_CHARS[((int)(input[j] >>> (1<<2))) & 0xF];
+			sb[(j<<1)|1] = HEX_CHARS[((int)(input[j] >>> (0<<2))) & 0xF];
 		}
 		return new String(sb);
 	}
