8a60841e4006f52e601284d9a0dffdc15e791f75
ggrandes <guillermo.grandes@gmail.com>
2013-08-02 20:08:33 +0200

    How check if JVM is 64 bits?

--- src/Check64bitsJVM.java
+++ src/Check64bitsJVM.java
@@ -1,18 +1,32 @@
+import java.lang.management.ManagementFactory;
+
 public class Check64bitsJVM {
 	public static void main(final String[] args) throws Throwable {
 		System.out.println("JVM is 64bits?: " + JVMis64bits());
 	}
+
 	public static boolean JVMis64bits() {
-		final String propOsArch = System.getProperty("os.arch");
-		final String propSunDataModel = System.getProperty("sun.arch.data.model");
-		System.out.println("os.arch: " + propOsArch);
-		System.out.println("sun.arch.data.model: " + propSunDataModel);
 		// http://stackoverflow.com/questions/807263/how-do-i-detect-which-kind-of-jre-is-installed-32bit-vs-64bit
-		if (propSunDataModel != null) {
-			return propSunDataModel.equals("64");
+		try {
+			final String propSunDataModel = System.getProperty("sun.arch.data.model");
+			if (propSunDataModel != null) {
+				return propSunDataModel.equals("64");
+			}
+		} catch (Exception ign) {
 		}
-		if (propOsArch != null) {
-			return propOsArch.contains("64");
+		try {
+			final String propOsArch = System.getProperty("os.arch");
+			if (propOsArch != null) {
+				return propOsArch.contains("64");
+			}
+		} catch (Exception ign) {
+		}
+		try {
+			final String jmxArchJMX = ManagementFactory.getOperatingSystemMXBean().getArch();
+			if (jmxArchJMX != null) {
+				return jmxArchJMX.contains("64");
+			}
+		} catch (Exception ign) {
 		}
 		return false;
 	}
