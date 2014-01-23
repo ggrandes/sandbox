9fa0cac7625a280aaf01dadc585118a2a0f66bab
ggrandes <guillermo.grandes@gmail.com>
2014-01-23 15:58:27 +0100

    High Strength Password Generator

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -2,7 +2,7 @@ import java.security.SecureRandom;
 
 /**
  * Normas para los Passwords:
- * <ul> 
+ * <ul>
  * <li>Al menos 8 characters</li>
  * <li>Al menos 1 caracter de cada grupo:</li>
  * <ul>
@@ -10,20 +10,22 @@ import java.security.SecureRandom;
  * <li>Letras minusculas <b>[a-z]</b></li>
  * <li>Numeros <b>[0-9]</b></li>
  * <li>Caracteres especiales: <b><[{(#$%&*?!:.,=+-_~^)}]></b></li>
- * </ul>
+ * <ul>
  * <li>No espacios</li>
  * <li>No letras acentuadas</li>
  * <li>No partes del username o ID</li>
  * </ul>
- *
+ * </ul>
+ * 
  * <a href="http://en.wikipedia.org/wiki/Password_strength">Password strength</a>
  * <a href="http://howsecureismypassword.net/">How Secure is My Password?</a>
  */
 public class PasswordGenerator {
 	// Excluded visual similar chars 01Ol
-	private final static char[] DEF_ALPHABET = "23456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz#$!:.=+-/_".toCharArray();
+	private final static char[] DEF_ALPHABET = ("23456789" + "ABCDEFGHIJKLMNPQRSTUVWXYZ"
+			+ "abcdefghijkmnopqrstuvwxyz" + "#$!:.=+-/_").toCharArray();
 	private final static SecureRandom r = new SecureRandom();
-	//
+
 	public static String genPassword(final int len) {
 		final char[] sb = new char[len];
 		while (true) {
@@ -32,15 +34,18 @@ public class PasswordGenerator {
 				final int j = ((r.nextInt() & 0x7FFFFFFF) % DEF_ALPHABET.length);
 				final char c = DEF_ALPHABET[j];
 				// No char repeat
-				if ((i > 0) && (c == sb[i-1])) {
+				if ((i > 0) && (c == sb[i - 1])) {
 					continue;
 				}
-				if ((c >= '0') && (c <= '9')) nums = true;
-				else if ((c >= 'a') && (c <= 'z')) minus = true;
-				else if ((c >= 'A') && (c <= 'Z')) mayus = true;
-				else {
+				if ((c >= '0') && (c <= '9')) {
+					nums = true;
+				} else if ((c >= 'a') && (c <= 'z')) {
+					minus = true;
+				} else if ((c >= 'A') && (c <= 'Z')) {
+					mayus = true;
+				} else {
 					// Don't begin/end with sign
-					if ((i == 0) || (i == len-1)) {
+					if ((i == 0) || (i == len - 1)) {
 						continue;
 					}
 					sign = true;
@@ -52,6 +57,7 @@ public class PasswordGenerator {
 				return (new String(sb));
 		}
 	}
+
 	public static void main(final String[] args) {
 		for (int i = 8; i <= 64; i *= 2) {
 			System.out.println(i + "\t" + genPassword(i));
@@ -60,5 +66,4 @@ public class PasswordGenerator {
 			System.out.println(i + "\t" + genPassword(i));
 		}
 	}
-
 }
